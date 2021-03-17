2d04d27882613ad4a60fdb5a0ce48c30263e395c
DESKTOP-INGCJVO\desktop <flynnspixel@gmail.com>
2021-03-17 18:46:51 +1100

    add initial template project to web

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 
 // We have to emulate __dirname as we are using "module" as opposed to "commonjs"
-// https://nodejs.org/api/esm.html#esm_no_require_exports_module_exports_filename_dirname
+// https://github.com/nodejs/help/issues/2907
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
---
+++ packages/web/src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Survey } from "@compass-surveys/common";
+
+function App() {
+  return <div className="App">Blank page</div>;
+}
+
+export default App;
