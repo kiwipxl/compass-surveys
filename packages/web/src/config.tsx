fd4e9834cf8c572bd0f1af649215dd4e17dd7267
DESKTOP-INGCJVO\desktop <flynnspixel@gmail.com>
2021-03-21 23:13:47 +1100

    set a specific heroku environment variable

--- packages/web/src/config.tsx
+++ packages/web/src/config.tsx
@@ -1,11 +1,6 @@
 import process from 'process';
 
-let url: string = '';
-if (process.env.NODE_ENV === 'production') {
-  url = 'http://localhost/api';
-} else {
-  url = 'http://localhost:4000/api/';
-}
-export const SERVER_URL = url;
+export const SERVER_URL =
+  process.env.WEB_SERVER_URL || 'http://localhost:4000/api/';
 
 console.log(`Using server url at ${SERVER_URL}`);
