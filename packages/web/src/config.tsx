55da8799b26e41d665d7a6e2c1358a3899f560ba
DESKTOP-INGCJVO\desktop <flynnspixel@gmail.com>
2021-03-21 23:02:53 +1100

    try this as server url

--- packages/web/src/config.tsx
+++ packages/web/src/config.tsx
@@ -1,1 +1,11 @@
-export const SERVER_URL = 'http://localhost:4000/api/';
+import process from 'process';
+
+let url: string = '';
+if (process.env.NODE_ENV === 'production') {
+  url = 'http://localhost/api';
+} else {
+  url = 'http://localhost:4000/api/';
+}
+export const SERVER_URL = url;
+
+console.log(`Using server url at ${SERVER_URL}`);
